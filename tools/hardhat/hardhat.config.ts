c10ca8b4ad230a64b1f5702c4fe844bea8072512
Dmitry <dymitr.paremski@thesis.co>
2024-11-21 15:00:19 +0100

    Removing read priv keys from local path
    Private keys should be set via 'npx hardhat vars set MEZO_ACCOUNTS'
    command.

--- tools/hardhat/hardhat.config.ts
+++ tools/hardhat/hardhat.config.ts
@@ -1,13 +1,7 @@
 import { vars, HardhatUserConfig } from 'hardhat/config'
-import { ethers } from 'ethers'
 import '@nomicfoundation/hardhat-toolbox'
 // import precompile tasks
 import './tasks/validatorpool'
-import fs from 'fs'
-import path from 'path'
-
-const BUILD_DIR = '../../.localnet/'
-const COUNT = 4
 
 function getPrivKeys (): string[] {
   const strings: string[] = vars.get('MEZO_ACCOUNTS', '').split(',')
@@ -20,12 +14,8 @@ function getPrivKeys (): string[] {
       }
     }
   } else {
-    for (let i = 0; i < COUNT; i++) {
-      const filePath = path.resolve(`${BUILD_DIR}node${i}/mezod/key_seed.json`)
-      const seed = JSON.parse(fs.readFileSync(filePath, 'utf8'))
-      const pk: string = ethers.Wallet.fromPhrase(seed.secret).privateKey
-      keys.push(pk)
-    }
+    console.log('MEZO_ACCOUNTS not set')
+    return []
   }
 
   return keys
